bd81dc40c3a6081ef7688bd252690f3cce809b9e
AdelaideCreative <david@adelaidecreative.com.au>
2018-02-11 12:36:34 +1030

    updated urls for API

--- app/src/main/java/com/breadwallet/tools/manager/BRApiManager.java
+++ app/src/main/java/com/breadwallet/tools/manager/BRApiManager.java
@@ -169,7 +169,7 @@ public class BRApiManager {
 
 
     public static JSONArray fetchRates(Activity activity) {
-        String jsonString = urlGET(activity, "https://litecoin.com/api/v1/ticker");
+        String jsonString = urlGET(activity, "https://api.strayawallet.com/rates/");
         JSONArray jsonArray = null;
         if (jsonString == null) return null;
         try {
@@ -182,7 +182,7 @@ public class BRApiManager {
     }
 
     public static JSONArray backupFetchRates(Activity activity) {
-        String jsonString = urlGET(activity, "https://bitpay.com/rates");
+        String jsonString = urlGET(activity, "https://api.strayawallet.com/rates/");
 
         JSONArray jsonArray = null;
         if (jsonString == null) return null;
@@ -197,7 +197,7 @@ public class BRApiManager {
     }
 
     public static void updateFeePerKb(Activity activity) {
-        String jsonString = urlGET(activity, "https://api.loafwallet.org/fee-per-kb");
+        String jsonString = urlGET(activity, "https://api.strayawallet.com/fee-per-kb");
         if (jsonString == null || jsonString.isEmpty()) {
             Log.e(TAG, "updateFeePerKb: failed to update fee, response string: " + jsonString);
             return;
